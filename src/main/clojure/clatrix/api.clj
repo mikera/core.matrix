212dc5a988a96f1bf1a756f74eca09740aabd5bf
mikera <mike.r.anderson.13@gmail.com>
2013-01-03 11:21:18 +0800

    Fixup tests to pass

--- src/main/clojure/clatrix/api.clj
+++ src/main/clojure/clatrix/api.clj
@@ -1,7 +1,19 @@
 (ns clatrix.api)
 
 (defprotocol PIndexedAccess
-  (get [x])
-  (get [x y])
-  (get-multi-dim [indexes]))
+  (mget-1 [m x])
+  (mget-2 [m x y])
+  (mget-multi-dim [m indexes]))
 
+(extend-protocol PIndexedAccess
+  clojure.lang.IPersistentVector
+    (mget-1 [m x]
+      (double (.nth m (int x))))
+    (mget-2 [m x y]
+      (let [row (.nth m (int x))]
+        (mget-1 row y)))
+    (mget-multi-dim [m indexes]
+      (if-let [next-indexes (next indexes)]
+        (let [m (.nth m (int (first indexes)))]
+          (mget-multi-dim m next-indexes))
+        (double (.nth m (int (first indexes)))))))
--- src/test/clojure/clatrix/test_api.clj
+++ src/test/clojure/clatrix/test_api.clj
@@ -1,3 +1,8 @@
 (ns clatrix.test-api
-  (:use clojure.test))
+  (:use clojure.test)
+  (:use clatrix.api))
 
+(deftest test-indexed-access
+  (testing "clojure vector indexed access"
+     (is (== 1 (mget-1 [1 2 3] 0)))
+     (is (== 1 (mget-2 [[1 2 3] [4 5 6]] 0 0)))))
