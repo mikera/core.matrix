6515569101bd7e21fa6932843536e0b534748260
mikera <mike.r.anderson.13@gmail.com>
2013-01-05 13:05:03 +0800

    Matrix operator tests

---
+++ src/test/clojure/core/matrix/test_operators.clj
@@ -0,0 +1,31 @@
+(ns core.matrix.test-operators
+  (:use clojure.test)
+  (:use core.matrix)
+  (:use core.matrix.operators)
+  (:require core.matrix.impl.persistent-vector)
+  (:refer-clojure :exclude [vector? * - + /]))
+
+(deftest test-multiply
+  (testing "scalars"
+    (is (== 6 (* 3 2))))
+  (testing "matrix scaling"
+    (is (= [6.0] (* [3] 2)))
+    (is (= [6.0] (* 2 [3])))
+    (is (= [[6.0]] (* 2 [[3]])))
+    (is (= [[6.0]] (* [[2]] 3)))))
+
+(deftest test-addition
+  (testing "scalars"
+    (is (== 5 (+ 3 2))))
+  (testing "matrix addition"
+    (is (= [5.0] (+ [3.0] [2.0])))
+    (is (= [[6.0]] (+ [[2.0]] [[4.0]])))
+    (is (= [[[6.0]]] (+ [[[2.0]]] [[[4.0]]])))))
+
+(deftest test-subtraction
+  (testing "scalars"
+    (is (== 5 (- 7 2))))
+  (testing "matrix subtraction"
+    (is (= [1.0] (- [3.0] [2.0])))
+    (is (= [[8.0]] (- [[12.0]] [[4.0]])))
+    (is (= [[[8.0]]] (- [[[12.0]]] [[[4.0]]])))))
---
+++ src/test/java/clatrix/OperatorTest.java
@@ -0,0 +1,11 @@
+package clatrix;
+
+import mikera.cljunit.NamespaceTest;
+
+public class OperatorTest extends NamespaceTest {
+	
+	@Override
+	public String namespace() {
+		return "core.matrix.test-operators";
+	}
+}
