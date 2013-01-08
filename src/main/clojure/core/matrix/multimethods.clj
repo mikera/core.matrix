8959c304bd6be1ebefdbf1ae107540d97c58b9f5
mikera <mike.r.anderson.13@gmail.com>
2013-01-08 12:37:50 +0800

    docs

--- src/main/clojure/core/matrix/multimethods.clj
+++ src/main/clojure/core/matrix/multimethods.clj
@@ -1,7 +1,14 @@
 (ns core.matrix.multimethods)
 
+;; multimethods are provided for when an implementation does not have a specific fast path
+;; for performing a matrix operation. Enables several options:
+;; - other implementations to provide a better handler
+;; - a default implementation may be used
+
 (defmulti mul
   (fn [x y] [(.getClass x) (.getClass y)]))
+
+;; heirarchies for matrix types
 
 (derive ::square-matrix ::matrix)
 (derive ::diagonal-matrix ::square-matrix)
