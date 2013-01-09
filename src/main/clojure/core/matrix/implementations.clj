4bcc2cbf364a429c1a95d51f9b7d130125782d39
mikera <mike.r.anderson.13@gmail.com>
2013-01-09 16:01:07 +0800

    Add more implementations in TODO form

--- src/main/clojure/core/matrix/implementations.clj
+++ src/main/clojure/core/matrix/implementations.clj
@@ -2,11 +2,15 @@
 
 ;; map of known implementation tags to namespace imports
 
+(def DEFAULT-IMPLEMENTATION :perstistent-vector)
+
 (def KNOWN-IMPLEMENTATIONS
   {:vectorz 'mikera.vectorz.matrix-api
    :ndarray 'core.matrix.impl.ndarray
-   :default 'core.matrix.impl.persistent-vector
+   :persistent-vector 'core.matrix.impl.persistent-vector
    :jblas :TODO
    :clatrix :TODO
    :parallel-colt :TODO
-   :ejml :TODO})
+   :ejml :TODO
+   :ujmp :TODO
+   :commons-math :TODO})
