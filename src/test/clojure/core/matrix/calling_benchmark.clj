682a22c4e6fd9fb2042a73a6765fcb5251814cf3
mikera <mike.r.anderson.13@gmail.com>
2013-01-16 11:18:37 +0800

    Improve calling convention benchmarks

--- src/test/clojure/core/matrix/calling_benchmark.clj
+++ src/test/clojure/core/matrix/calling_benchmark.clj
@@ -1,23 +1,28 @@
 (ns core.matrix.calling-benchmark
   (:require [criterium.core :as c]))
 
+;; benchmark for a few different calling conventions
+
 (set! *unchecked-math* true)
 (set! *warn-on-reflection* true)
 
-;; benchmark for a few different calling conventions
-
+;; an array to mutate: gives methods something to do with a side effect
 (def arr (long-array 1))
 
+;; Java interface
 (definterface IInterface
   (interfaceCall [i])
   (interfaceCallPrim [^long i]))
 
+;; Protocol
 (defprotocol POperation
   (proto-call [m i]))
 
+;; Protocol for extending later
 (defprotocol PExtendedOperation
   (extended-call [m i]))
 
+;; Clojure detype implementing POperation and IInterface
 (deftype MyType []
   POperation
     (proto-call [m i]
@@ -28,18 +33,29 @@
     (interfaceCallPrim [m ^long i]
       (aset ^longs arr 0 i)))
 
+;; extend PExtendedOperation to MyType *after* it is defined
+;; so we can determine extra overhead of extending
 (extend-protocol PExtendedOperation
   MyType
     (extended-call [m i]
       (aset ^longs arr 0 (long i)))) 
 
-(def my-type (MyType.))
+;; an instance of MyType
+(def ^MyType my-type (MyType.))
 
 (defn prim-call [^long x]
   (aset ^longs arr 0 x) nil)
 
 (defn boxed-call [^Long x]
   (aset ^longs arr 0 (long x)) nil)
+
+(defmulti class-multimethod (fn [m i] (class m)))
+(defmethod class-multimethod MyType [m i] (aset ^longs arr 0 (long i)))
+(defmethod class-multimethod :default [m i] (throw (IllegalArgumentException.)))
+
+(defmulti double-multimethod (fn [m i] [(class m) (class i)]))
+(defmethod double-multimethod [MyType Long] [m i] (aset ^longs arr 0 (long i)))
+(defmethod double-multimethod :default [m i] (throw (IllegalArgumentException.)))
 
 ;; =============================================================
 ;; Benchmark code follows
@@ -65,18 +81,25 @@
 
 (c/quick-bench (dotimes [i 1000] (proto-call my-type i)))
 ;; 7.95 us
-;; same as regular boxed method call
+;; fast - similar to regular boxed method call
 
 (c/quick-bench (dotimes [i 1000] (extended-call my-type i)))
 ;; 13.81 us
-;; more expensive than regular protocol call
+;; quite fast - more expensive than regular protocol call
 
 (c/quick-bench (dotimes [i 1000] (.interfaceCall ^IInterface my-type i)))
 ;; 6.93 us
-;; pretty fast
+;; fast
 
 (c/quick-bench (dotimes [i 1000] (.interfaceCallPrim ^IInterface my-type i)))
 ;; 2.28 us
 ;; very fast!
 
+(c/quick-bench (dotimes [i 1000] (class-multimethod my-type i)))
+;; 88.98 us
+;; slower 
+
+(c/quick-bench (dotimes [i 1000] (double-multimethod my-type i)))
+;; 231.00 us
+;; much slower! still better than reflection though....
 )
