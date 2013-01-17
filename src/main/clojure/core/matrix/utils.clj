8bb5be3249c9be4c8ba50c9d4f2841a635493e88
mikera <mike.r.anderson.13@gmail.com>
2013-01-17 11:49:33 +0800

    doc updates

--- src/main/clojure/core/matrix/utils.clj
+++ src/main/clojure/core/matrix/utils.clj
@@ -1,5 +1,9 @@
 (ns core.matrix.utils)
 
+;; these are copies of methods from the library
+;;   https://github.com/mikera/clojure-utils
+;;
+;; duplicated here to avoid an extra dependency
 
 (defmacro error
   "Throws an error with the provided message(s)"
