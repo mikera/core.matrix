b9561be53621b0276e54226b788361f98dd642d1
mikera <mike.r.anderson.13@gmail.com>
2013-01-18 15:15:00 +0800

    Add some example code

--- src/main/clojure/core/matrix/examples.clj
+++ src/main/clojure/core/matrix/examples.clj
@@ -1,14 +1,27 @@
 (ns core.matrix.examples
-  (:use core.matrix))
+  (:use core.matrix)
+  (:use core.matrix.operators)
+  (:refer-clojure :exclude [* - +]))
 
 ;; set core core.matrix examples
 
 (defn all-examples []
+  ;; a matrix can be defined using a nested vector
+  (def a (matrix [[2 0] [0 2]]))
+
+  ;; a matrix can be a pure clojure value 
+  a
   
+  ;; core.matrix.operators overloads operators to work on matrices
+  (* 2 a)
   
+  ;; a range of mathematical functions are defined for matrices
+  (sqrt a)  
   
+  ;; you can get rows and columns of matrices individually
+  (get-row a 0)
   
-  
-  
+  ;; Java double arrays can be used as vectors
+  ;;(* a (double-array [1 2]))
 )
 
