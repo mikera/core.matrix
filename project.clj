bf36e492b4e6ad3587a4818016081140ad71d236
Joel Boehland <joel.boehland@evocomputing.com>
2013-01-28 13:24:46 -0800

    Sync project.clj with pom.xml

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mikera/matrix-api "0.0.2-SNAPSHOT"
+(defproject mikera/core.matrix "0.0.8-SNAPSHOT"
   :url "https://github.com/mikera/matrix-api"
   :license {:name "Eclipse Public License (EPL)"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,5 +7,5 @@
   :test-paths ["src/test" "src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[com.google.caliper/caliper "0.5-rc1"]
-                                  [net.mikera/cljunit "0.1.4"]
+                                  [net.mikera/cljunit "0.1.5"]
                                   [criterium/criterium "0.3.1"]]}})
