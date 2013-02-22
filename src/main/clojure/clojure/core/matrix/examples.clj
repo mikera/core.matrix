64ed51b9028ac5a626d1d3af292654c1bf44e62b
mikera <mike.r.anderson.13@gmail.com>
2013-02-22 16:59:32 +0800

    improved examples

--- src/main/clojure/clojure/core/matrix/examples.clj
+++ src/main/clojure/clojure/core/matrix/examples.clj
@@ -1,7 +1,7 @@
 (ns clojure.core.matrix.examples
-  (:refer-clojure :exclude [* - +])
   (:use clojure.core.matrix)
-  (:use clojure.core.matrix.operators))
+  (:use clojure.core.matrix.operators)
+  (:refer-clojure :exclude [* - +]))
 
 ;; =============================================================
 ;; Set of clojure.core.matrix examples
@@ -10,13 +10,15 @@
   ;; a matrix can be defined using a nested vector
   (def a (matrix [[2 0] [0 2]]))
 
-  ;; a matrix can be a pure clojure value 
+  ;; a matrix is just a regular clojure value 
   a
   
   ;; clojure.core.matrix.operators overloads operators to work on matrices
-  (* a a)
+  ;; example: matrix x matrix multiplication
+  (* a [[1 2] [3 4]])
   
   ;; a range of mathematical functions are defined for matrices
+  ;; these return new matrices
   (sqrt a)  
   
   ;; you can get rows and columns of matrices individually
@@ -26,14 +28,16 @@
   (* a (double-array [1 2]))
   
   ;; you can also modify double arrays in place - they are examples of mutable vectors
-  (let [a (double-array [1 4 9])]
-    (sqrt! a)
-    (seq a))
+  (let [v (double-array [1 4 9])]
+    (sqrt! v)
+    (vec v))  ;; use (vec ...) to display the contents of the array
   
-  ;; you can coerce matrices between different types
-  (coerce [] (double-array [1 2 3]))
+  ;; you can coerce matrices between different formats. Use either an example of the desired 
+  ;; type or a keyword identifier to specify what type of matrix/vector you want.
+  (coerce [] '(1 2 3 4))
+  (coerce :double-array [1 2 3])
   
-  ;; scalars can be used in many places that you can use a matrix
+  ;; scalar values can be used in many places that you can use a matrix
   (* [1 2 3] 2)
   
   ;; operations on scalars alone behave as you would expect
@@ -42,5 +46,10 @@
   ;; you can do various functional programming tricks with matrices too
   (emap inc [[1 2] [3 4]])
   
+  ;; you can access parts of a vector with subvector
+  (subvector [1 2 3 4 5 6] 2 3) 
+  
+  ;; various special matrix constructors are provided
+  (identity-matrix 3) 
 )
 
