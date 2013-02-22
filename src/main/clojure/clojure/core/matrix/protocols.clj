5dee4d2c1b26b63e0c71bf676105e5eecbeb05bc
mikera <mike.r.anderson.13@gmail.com>
2013-02-22 15:03:39 +0800

    Add subvector protocol

--- src/main/clojure/clojure/core/matrix/protocols.clj
+++ src/main/clojure/clojure/core/matrix/protocols.clj
@@ -157,6 +157,12 @@
   (get-major-slice [m i])
   (get-slice [m dimension i]))
 
+(defprotocol PSubVector
+  "Protocol for getting a sub-vector view of a vectot. Must return a mutable view
+   if the original vector is mutable. Should throw an exception if the specified 
+   subvector is out of bounds for the target vector."
+  (subvector [m start length])) 
+
 (defprotocol PSliceView
   "Protocol for quick view access into a row-major slices of an array. If implemented, must return either a view
    or an immutable sub-matrix: it must *not* return copied data. 
