6e577b84c3525652c2fe67a7e473287830544227
Jochen Goertler <lyzrd17@gmail.com>
2013-03-10 18:00:10 +0100

    Revert back to clojure 1.4

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/test/java"]
   :test-paths ["src/test/clojure" "src/test/java"]
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[com.google.caliper/caliper "0.5-rc1"]
                                   [net.mikera/cljunit "0.2.0"]
                                   [criterium/criterium "0.3.1"]]}})
