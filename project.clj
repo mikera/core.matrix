db1597c6b97fe09fe31c27e801ac2a0fb8b73159
mikera <mike.r.anderson.13@gmail.com>
2013-03-10 18:07:34 +0800

    attempt to fix lein source paths

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :url "https://github.com/mikera/matrix-api"
   :license {:name "Eclipse Public License (EPL)"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/main"]
+  :source-paths ["src/main/clojure"]
   :java-source-paths ["src/test/java"]
-  :test-paths ["src/test" "src/test"]
+  :test-paths ["src/test/clojure" "src/test/java"]
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[com.google.caliper/caliper "0.5-rc1"]
                                   [net.mikera/cljunit "0.2.0"]
