d6f64e970efd38e4c2b342105b514193e981bc08
mikera <mike.r.anderson.13@gmail.com>
2013-04-07 19:22:47 +0800

    sparse-map tests

--- src/test/clojure/clojure/core/matrix/test_sparse_map.clj
+++ src/test/clojure/clojure/core/matrix/test_sparse_map.clj
@@ -4,7 +4,7 @@
   (:require [clojure.core.matrix.operators :as op])
   (:require [clojure.core.matrix.impl.wrappers :as wrap])
   (:require [clojure.core.matrix.compliance-tester])
-  (:require clojure.core.matrix.impl.sparse-map))
+  (:require [clojure.core.matrix.impl.sparse-map :as sm]))
 
 (defn sm 
   [data]
@@ -12,7 +12,9 @@
 
 (deftest test-sparse-map-construct
   (let [m (sm [[1 2] [3 4]])]
-    (is (== 2 (dimensionality m)))))
+    (is (== 2 (dimensionality m)))
+    (is (e= [[1 nil] [nil 1]] ^{:shape [2 2]} {[0 0] 1 [1 1] 1}))
+    (is (equals [[1 0] [0 1]] ^{:shape [2 2] :default-value 0} {[0 0] 1 [1 1] 1}))))
 
 (deftest instance-tests
   (testing "matrices of symbols are supported"
