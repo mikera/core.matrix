ec860f256cdac9b14db760a97c9869b5893556df
John Downs <john.a.downs@gmail.com>
2013-06-05 14:26:00 -0300

    Competed tests for multiply and add

--- src/test/clojure/clojure/core/matrix/test_row_operators.clj
+++ src/test/clojure/clojure/core/matrix/test_row_operators.clj
@@ -1,3 +1,8 @@
+(ns clojure.core.matrix.test-api
+  (:use clojure.test)
+  (:use clojure.core.matrix.row_operators))
+
+
 (deftest test-swap
   (testing "vector row swap"
     (is (== [0 2] (swap [2 0] 0 1)))
@@ -7,5 +12,10 @@
     (is (== [[0 2] [2 0]] (swap [[2 0] [0 2]] 0 1)))
     (is (== [[0 2] [2 0] [1 1]] (swap [[1 1] [2 0] [0 2]] 0 2)))))
 
-(deftest test-mul)
+(deftest test-multiply
+  (testing "multiply row i by constant k"
+    (is (== [0 2 4] (multiply [[0 1 2]] 2)))))
 
+(deftest test-add
+  (testing "add row j to i and replace i with the result"
+    (is (== [[3 3] [1 1]] (add [[1 1] [1 1]] 0 1 2 )))))
