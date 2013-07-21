2e9abae48150e7a63b34e60af75edc75608b8c1a
Dmitry Groshev <lambdadmitry@gmail.com>
2013-07-21 15:50:38 +0400

    add matrix multiplication benchmark

--- src/dev/clojure/clojure/core/matrix/docgen/bench.clj
+++ src/dev/clojure/clojure/core/matrix/docgen/bench.clj
@@ -1,5 +1,6 @@
 (ns clojure.core.matrix.docgen.bench
   (:require [criterium.core :as cr]
+            [clojure.core.matrix :as m]
             [clojure.core.matrix.protocols :as mp]
             [clojure.core.matrix.implementations :as imp]
             [clojure.core.matrix.multimethods :as mm]
@@ -10,11 +11,27 @@
            :reference :vectorz
            :fast-bench true})
 
-(defn bench-proto [proto]
-  ()
-  )
+(defn rand-mtx
+  ([n] (rand-mtx n 1000 identity))
+  ([n max caster]
+     (letfn [(rand-row [] (->> #(rand-int max)
+                               repeatedly (map caster) (take n) vec))]
+       (->> rand-row repeatedly (take n) vec))))
 
-(defn generate []
-  (let [protos (c/extract-protocols)]
-
-    ))
+(defn mmultiply-bench []
+  (let [n 50]
+    (println "Test matrix size:" n)
+    (println "Vectorz:")
+    (let [[ma mb] (->> #(rand-mtx n)
+                       repeatedly (map #(m/array :vectorz %)) (take 2))]
+      (cr/report-result (cr/quick-benchmark (mp/matrix-multiply ma mb) {})))
+    (println "-----")
+    (println "Persistent vectors:")
+    (let [[ma mb] (->> #(rand-mtx n)
+                       repeatedly (take 2))]
+      (cr/report-result (cr/quick-benchmark (mp/matrix-multiply ma mb) {})))
+    (println "-----")
+    (println "NDArray:")
+    (let [[ma mb] (->> #(rand-mtx n)
+                       repeatedly (map #(m/array :ndarray %)) (take 2))]
+      (cr/report-result (cr/quick-benchmark (mp/matrix-multiply ma mb) {})))))
