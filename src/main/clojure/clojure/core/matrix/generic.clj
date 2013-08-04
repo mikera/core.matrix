247b8f35b701893370a642fdf1cbd9a0409786fa
mikera <mike.r.anderson.13@gmail.com>
2013-08-04 09:16:50 +0100

    add placeholders for generic element accessor functions

--- src/main/clojure/clojure/core/matrix/generic.clj
+++ src/main/clojure/clojure/core/matrix/generic.clj
@@ -1,4 +1,22 @@
-(ns clojure.core.matrix.generic)
+(ns clojure.core.matrix.generic
+  (:use clojure.core.matrix.utils))
 
 ;; Placeholder namespace for generic versions of core.matrix algorithms
 
+;; generic element accessor functions
+
+(defn generic-zero
+  "Returns the standard 'zero' scalar value for the given array / implementation"
+  (TODO))
+
+(defn generic-one
+  "Returns the standard 'one' scalar value for the given array / implementation"
+  (TODO))
+
+(defn generic-add-fn
+  "Returns the standard 'add' function for the given array / implementation."
+  (TODO))
+
+(defn generic-mul-fn
+  "Returns the standard 'multiply' function for the given array / implementation."
+  (TODO))
