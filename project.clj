2c99107ab3627a585ce5c3b6953fcdf6e1ea6801
mikera <mike.r.anderson.13@gmail.com>
2013-08-22 17:11:18 +0800

    bump project.clj version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.mikera/core.matrix "0.7.2"
+(defproject net.mikera/core.matrix "0.9.0"
   :url "https://github.com/mikera/matrix-api"
   :license {:name "Eclipse Public License (EPL)"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
