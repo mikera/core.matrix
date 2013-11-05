db25af628e8572fb0cdcce86599e1d7363a8e7e3
mikera <mike.r.anderson.13@gmail.com>
2013-11-05 19:32:19 +0800

    inline benchmark edis

--- src/test/clojure/clojure/core/matrix/inline_benchmark.clj
+++ src/test/clojure/clojure/core/matrix/inline_benchmark.clj
@@ -5,7 +5,7 @@
 ;; benchmark for inline usage
 
 (set! *unchecked-math* true)
-(set! *warn-on-reflection* true)
+;; (set! *warn-on-reflection* true)
 
 (defn add-fn
   ([x y]
@@ -31,23 +31,41 @@
   ([x]
     (.length x)))
 
+
+(defn count-fn
+  ([x] (count x)))
+
+(defn count-inline
+  {:inline (fn ([x] `(count ~x)))}
+  ([x] (count x)))
+
 (comment
-  ;; normal fn - 23ns per call
+  ;;========================================================
+  ;; normal fn - 23.3 ns per call
   (c/quick-bench (dotimes [i 1000] (add-fn i 10)))
 
   ;; primitive fn - 1.25 ns per call
   (c/quick-bench (dotimes [i 1000] (add-prim i 10)))
   
-  ;; inline fn - 1.2 ns per call
+  ;; inline fn - 1.19 ns per call
   (c/quick-bench (dotimes [i 1000] (add-inline i 10)))
   
   
+  ;;========================================================
   ;; fn with relection - 2060 ns per call
   (c/quick-bench (dotimes [i 1000] (length-fn "foo")))
 
-  ;; fn with type hints - 0.9 ns per call
+  ;; fn with type hints - 0.90 ns per call
   (c/quick-bench (dotimes [i 1000] (length-hinted "foo")))
   
-  ;; inline fn - 0.6 ns per call
+  ;; inline fn - 0.60 ns per call
   (c/quick-bench (dotimes [i 1000] (length-inline "foo")))
+  
+  
+  ;;========================================================
+  ;; wrapped fn count: 1.50 ns per call    (why is this so fast?!?)
+  (c/quick-bench (dotimes [i 1000] (count-fn [1 2 3])))
+
+  ;; inline count: 0.61 ns per call
+  (c/quick-bench (dotimes [i 1000] (count-inline [1 2 3])))
  )
