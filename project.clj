0d5f97a0de1f6c6f92ed7c4ea3c24a56d210fc58
mabrams <mabrams@uab.edu>
2014-01-04 12:49:49 -0600

    Copied pom.xml, project.clj from mikera.core.matrix develop

--- project.clj
+++ project.clj
@@ -1,22 +1,23 @@
-(defproject mars0i/core.matrix "0.18.1-SNAPSHOT"
-  :description "mars0i's fork of mikera/core.matrix"
-  :url "https://github.com/mars0i/core.matrix"
+;; This project.clj is provided as a convenience for Leiningen users
+;;
+;; The official core.matrix project configuration is in the pom.xml
+;; dependencies / configuration in this file may be out of date
+;; if in doubt, please refer to the latest pom.xml
+
+(defproject net.mikera/core.matrix "0.17.0"
+  :url "https://github.com/mikera/matrix-api"
   :license {:name "Eclipse Public License (EPL)"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/test/java"]
   :test-paths ["src/test/clojure" "src/test/java"]
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :marginalia {:javascript ["http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"]}
   :profiles {:dev {:dependencies [[com.google.caliper/caliper "0.5-rc1"]
                                   [net.mikera/cljunit "0.3.0"]
                                   [criterium/criterium "0.4.2"]
-                                  [hiccup "1.0.4"]
-                                  [mars0i/vectorz-clj "0.17.1-SNAPSHOT"]
-                                  [mars0i/clatrix "0.4.0-SNAPSHOT"]
-                                  [reiddraper/simple-check "0.4.1"]
-                                  [org.clojure/tools.macro "0.1.5"]
-                                  [com.google.caliper/caliper "0.5-rc1"]
+                                  [hiccup "1.0.3"]
+                                  [net.mikera/vectorz-clj "0.17.1"]
                                   [reiddraper/simple-check "0.5.3"]]
                    :source-paths ["src/main/clojure" "src/dev/clojure"]
                    :jvm-opts ^:replace []}})
