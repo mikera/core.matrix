c2c8e287f6e2aa768b74b68fa657b828bc2e6e7e
mikera <mike.r.anderson.13@gmail.com>
2014-04-05 16:21:41 +0800

    extra tests

--- src/test/clojure/clojure/core/matrix/test_pprint.clj
+++ src/test/clojure/clojure/core/matrix/test_pprint.clj
@@ -8,7 +8,8 @@
 
 (deftest test-vectorz-pm
   (is (pp/pm (array :vectorz [1 2])))
-  (is (pp/pm (array :vectorz [[1 2] [3 4]]))))
+  (is (pp/pm (array :vectorz [[1 2] [3 4]])))
+  (is (pp/pm (array :vectorz [[[1 2] [3 4]] [[5 6] [7 8]]]))))
 
 (deftest test-non-numeric
   (is (= "[:a foo bar  ]" (pp/pm [:a "foo" 'bar \space]))))
