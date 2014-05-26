328ea6fba8b58e7a6cf159045e2ab4254fd1114b
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2014-05-26 13:45:50 +0200

    qr decomposition doc string added

--- src/main/clojure/clojure/core/matrix/linear.clj
+++ src/main/clojure/clojure/core/matrix/linear.clj
@@ -4,5 +4,19 @@
 
 
 (defn qr
+  "Computes QR decomposition of a matrix.
+   Returns a map containing matrices of an input matrix type with the keys [:Q :R] such that:
+        M = Q.R
+
+   Where:
+    - Q is an orthogonal matrix
+    - R is an upper triangular matrix (= right triangular matrix)
+   If :return parameter is specified in options map, it returns only specified keys.
+
+   To provide a meaningful solution the original matrix must have full rank.
+
+   Intended usage: (let [{:keys [Q R]} (qr M)] ....)
+                   (let [{:keys [R]} (qr M {:return [:R]})] ....)"
+
   ([m options] (mp/qr m options))
   ([m] (mp/qr m {:return [:Q :R]})))
