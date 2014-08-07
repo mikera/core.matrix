122d3313be6e328c19cdff0714024366f1024e8b
mikera <mike.r.anderson.13@gmail.com>
2014-08-07 16:27:57 +0800

    tidy project.clj

--- project.clj
+++ project.clj
@@ -8,15 +8,20 @@
   :url "https://github.com/mikera/matrix-api"
   :license {:name "Eclipse Public License (EPL)"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/test/java"]
+  
   :test-paths ["src/test/clojure" "src/test/java"]
+  
   :dependencies [[org.clojure/clojure "1.6.0"]]
+  
   :marginalia {:javascript ["http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"]}
+  
   :profiles {:dev {:dependencies [[net.mikera/cljunit "0.3.1"]
                                   [com.google.caliper/caliper "0.5-rc1"]
                                   [criterium/criterium "0.4.3"]
-				  [org.clojure/tools.macro "0.1.5"]
+                                  [org.clojure/tools.macro "0.1.5"]
                                   [hiccup "1.0.5"]
                                   [net.mikera/vectorz-clj "0.23.0"]
                                   [org.clojure/test.check "0.5.8"]]
