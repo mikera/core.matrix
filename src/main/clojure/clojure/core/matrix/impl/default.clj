c8350d6178af37964fc52212cbd57127acd6053a
mikera <mike.r.anderson.13@gmail.com>
2014-08-28 07:43:21 +0800

    optimise orthogonality test default implementation

--- src/main/clojure/clojure/core/matrix/impl/default.clj
+++ src/main/clojure/clojure/core/matrix/impl/default.clj
@@ -1649,9 +1649,9 @@
   (orthogonal? [m eps]
     (and (square? m)
          (mp/matrix-equals-epsilon
-          (mp/matrix-multiply m (mp/transpose m))
-          (mp/identity-matrix m (first (mp/get-shape m)))
-          eps))))
+           (mp/matrix-multiply m (mp/transpose m))
+           (mp/identity-matrix m (mp/dimension-count m 0))
+           eps))))
 
 (extend-protocol mp/PSelect
   Object
