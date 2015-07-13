32c0423d1564d5befaa50c08820d720271d0a814
mikera <mike.r.anderson.13@gmail.com>
2015-07-13 18:09:01 +0800

    doc clarifications

--- src/main/clojure/clojure/core/matrix.clj
+++ src/main/clojure/clojure/core/matrix.clj
@@ -891,9 +891,8 @@
    otherwise slices along the first dimension. If the matrix implementation supports mutable views, these views
    can be used to mutate portions of the original array.
 
-   The key difference between 'slices' and 'slice-views' is that 'slice-views' will always return views, including
-   for the 0-dimensional case. Hence it will return a sequence of 0-dimensional scalar arrays if
-   the array is 1-dimensional."
+   The key difference between 'slices' and 'slice-views' is that 'slice-views' must always return views. In order 
+   to ensure this behaviour on mutable 1-dimensioanal arrays, it must return a sequence of 0-dimensioanal arrays."
   ([m]
     (mp/get-major-slice-view-seq m))
   ([m dimension]
