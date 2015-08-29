0ebb43aff809b4f4987a0a0343faa27dbf68195f
fyquah95 <quah.fy95@gmail.com>
2015-08-29 18:45:05 +0100

    Implement core protocols for double array 2d

--- src/main/clojure/clojure/core/matrix/impl/double_array_2d.clj
+++ src/main/clojure/clojure/core/matrix/impl/double_array_2d.clj
@@ -7,6 +7,17 @@
                      to-double-arrays]]))
 
 ; 2D double array implementation
+
+; TODO : Is it safe to assume that all columns have the
+; same length?
+
+; Utilities:
+
+(defn ^"[[D" copy-2d-double-array [^"[[D" m]
+  (into-array (Class/forName "[D")
+              (mapv copy-double-array ^"[[D" m)))
+
+; Protocol implementations
 (extend-protocol mp/PImplementation
   (Class/forName "[[D")
     (implementation-key [m] :double-array-2d)
@@ -49,5 +60,108 @@
                   (mapv copy-double-array m)))
     (as-double-array [m] m))
 
-(imp/register-implementation (make-array (Class/forName "[D") 1 1))
+(extend-protocol mp/PObjectArrayOutput
+  (Class/forName "[[D")
+    (to-object-array [m] (into-array Object
+                                     (mapv object-array m)))
+    (as-object-array [m] nil))
 
+(extend-protocol mp/PSummable
+  (Class/forName "[[D")
+    (element-sum [m]
+      (let [^"[[D" m m]
+        (areduce m i res-outer 0.0
+          (+ res-outer
+             (let [^doubles a (aget m i)]
+               (areduce a j res-inner 0.0
+                        (+ res-inner (aget a j)))))))))
+
+(extend-protocol mp/PIndexedAccess
+  (Class/forName "[[D")
+    (get-1d [m x]
+      (error "Cannot do get-1d from a 2D double array"))
+    (get-2d [m x y]
+      ^double (aget ^"[[D" m x y))
+    (get-nd [m indexes]
+      (if (== 2 (count indexes))
+        (let [[x y] indexes]
+          ^double (aget ^"[[D" m (int x) (int y)))
+        (error "Can't get from double array with dimensionality: " (count indexes)))))
+
+(extend-protocol mp/PIndexedSetting
+  (Class/forName "[[D")
+    (set-1d [m x v]
+      (error "Can't do 1D set on 2D double array"))
+    (set-2d [m x y v]
+      (let [^"[[D" mat (copy-2d-double-array m)]
+        (aset-double mat x y (double v))
+        mat))
+    (set-nd [m indexes v]
+      (if (== (count indexes) 2)
+        (let [^"[[D" mat (copy-2d-double-array m)
+              [x y] indexes]
+          (aset-double mat x y (double v))
+          mat) 
+        (error "Can't set on double array with dimensionality: " (count indexes))))
+    (is-mutable? [m] true))
+
+(extend-protocol mp/PIndexedSettingMutable
+  (Class/forName "[[D")
+    (set-1d! [m x v]
+      (error "Can't do 1D set on 2D double array"))
+    (set-2d! [m x y v]
+      (aset-double m x y (double v)))
+    (set-nd! [m indexes v]
+      (if (== (count indexes) 2)
+        (let [[x y] indexes]
+          (aset-double m x y (double v)))
+        (error "Can't set on double array with dimensionality: " (count indexes)))))
+
+(extend-protocol mp/PMutableMatrixConstruction
+  (Class/forName "[[D")
+    (mutable-matrix [m]
+      (copy-2d-double-array m)))
+
+(extend-protocol mp/PMatrixScaling
+  (Class/forName "[[D")
+    (scale [m a]
+      (let [m (copy-2d-double-array m)
+            x (alength m)
+            y (alength (aget m 0))]
+        (dotimes [i x]
+          (dotimes [j y]
+            (aset-double m i j (* a (aget m i j)))))
+        m))
+    (pre-scale [m a]
+      (let [m (copy-2d-double-array m)
+            x (alength m)
+            y (alength (aget m 0))]
+        (dotimes [i x]
+          (dotimes [j y]
+            (aset-double m i j (* a ^double (aget m i j)))))
+        m)))
+
+(extend-protocol mp/PMatrixMutableScaling
+  (Class/forName "[[D")
+    (scale! [m a]
+      (let [x (alength m)
+            y (alength (aget m 0))]
+        (dotimes [i x]
+          (dotimes [j y]
+            (aset-double m i j (* a ^double (aget m i j)))))))
+    (pre-scale! [m a]
+      (let [x (alength m)
+            y (alength (aget m 0))]
+        (dotimes [i x]
+          (dotimes [j y]
+            (aset-double m i j (* a ^double (aget m i j))))))))
+
+(extend-protocol mp/PConversion
+  (Class/forName "[[D")
+    (convert-to-nested-vectors [m]
+      (->> m
+           (mapv vec)
+           (mapv vec))))
+
+; registration
+(imp/register-implementation (make-array Double/TYPE 1 1))
