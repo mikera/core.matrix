e567d243f55226c84c0b56f599158fdfeda013c9
mikera <mike.r.anderson.13@gmail.com>
2015-09-25 08:28:28 +0800

    Docs for gemo.clj demo

--- src/test/clojure/clojure/core/matrix/demo/geom.clj
+++ src/test/clojure/clojure/core/matrix/demo/geom.clj
@@ -1,15 +1,22 @@
 (ns clojure.core.matrix.demo.geom
+  "Demonstration of geometric functionality using core.matrix"
   (:require [clojure.core.matrix :refer :all]))
 
+;; The Pi constant.
 (def π 3.141592653589793)
 
+;; The Tau constant. Better than Pi.
+;; http://tauday.com/tau-manifesto
 (def τ (* 2.0 π))
 
-(defn rot [turns]
-  (let [a (* τ turns)]
-    [[  (cos a)  (sin a)]
-     [(-(sin a)) (cos a)]]))
+(defn rot 
+  "Rotates a 2D vector by a given angle (in turns, i.e. 180 degrees = 0.5)"
+  ([turns]
+    (let [a (* τ turns)]
+      [[  (cos a)  (sin a)]
+       [(-(sin a)) (cos a)]])))
 
+;; Rotate a vector by a 1/8 turn
 (mmul (rot 1/8) [3 4])
 
 
