766ff360dcaf6948701d25395974121305d0516f
mikera <mike.r.anderson.13@gmail.com>
2015-11-03 17:07:23 +0800

    Extra vectorz tests on stats return type

--- src/test/clojure/clojure/core/matrix/test_vectorz.clj
+++ src/test/clojure/clojure/core/matrix/test_vectorz.clj
@@ -2,6 +2,7 @@
   (:require [clojure.core.matrix.impl.pprint :as pprint]
             [mikera.vectorz.matrix-api]
             [clojure.core.matrix.compliance-tester :as compliance]
+            [clojure.core.matrix.stats :as stats]
             [clojure.core.matrix :refer :all]
             [clojure.test :refer :all])
   (:import (mikera.arrayz INDArray)) )
@@ -11,16 +12,20 @@
 ;; Not intended to be exhasustive (since vectorz-clj has its own tests) however
 ;; this is useful to detect breaking changes early.
 
+(defmacro vectorz?
+  [x]
+  `(instance? INDArray ~x))
+
 (deftest regression-201
   (let [m (matrix :vectorz [[2 2][2 2]])
         mm (mutable m)]
-    (is (instance? INDArray mm))
+    (is (vectorz? mm))
     (is (equals [[4 4] [4 4]] (pow! mm 2)))))
 
 (deftest test-sparse
-  (is (instance? INDArray (sparse (matrix :vectorz [[1 2] [3 4]]))))
-  (is (instance? INDArray (sparse :vectorz [[1 2] [3 4]])))
-  (is (instance? INDArray (sparse-array :vectorz [[[1 2] [3 4]]]))))
+  (is (vectorz? (sparse (matrix :vectorz [[1 2] [3 4]]))))
+  (is (vectorz? (sparse :vectorz [[1 2] [3 4]])))
+  (is (vectorz? (sparse-array :vectorz [[[1 2] [3 4]]]))))
 
 (deftest test-pm
   (is (string? (pprint/pm (array :vectorz [1 2])))))
@@ -29,6 +34,10 @@
   (compliance/instance-test (array :vectorz [1 2 3]))
   (compliance/instance-test (array :vectorz [[1 2] [3 4]]))
   (compliance/instance-test (array :vectorz [[[1 2] [3 4]] [[5 6] [7 8]]])))
+
+(deftest test-stats
+  (is (vectorz? (stats/mean [(array :vectorz [1 2])
+                             (array :vectorz [5 4])]))))
 
 (deftest test-emap-indexed
   (is (equals [[1 12] 
