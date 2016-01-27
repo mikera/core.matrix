e74bf73a7a1520c5c46567e97155ca348a2a5faa
mikera <mike.r.anderson.13@gmail.com>
2016-01-27 10:06:47 +0800

    Update generators to use official test.check scale function

--- src/test/clojure/clojure/core/matrix/generators.clj
+++ src/test/clojure/clojure/core/matrix/generators.clj
@@ -11,19 +11,9 @@
 ;; ===========================================
 ;; Dimension, shape and scaling generators
 
-(defn gen-scale 
-  "Creates a generator that pre-modifies the 'size' pramater with the function f. Use if you want to 
-   have the size grow at a different rate from the normal linear scaling."
-  ([f gen]
-    (let [gf (or (:gen gen) "gen paramter must be a test.check generator")
-          new-gf (fn [rnd size]
-                   (gf rnd (f size)))]
-      (clojure.test.check.generators.Generator. new-gf))))
-
-
 (def gen-dims
   "A generator for dimensionalities (including zero). Grows quite slowly."
-  (gen-scale #(Math/pow (double %) 0.333) gen/pos-int))
+  (gen/scale #(Math/pow (double %) 0.333) gen/pos-int))
 
 (def gen-s-pos-dims
   "A generator for dimensionalities greated than or equal to one. Grows quite slowly."
@@ -37,7 +27,7 @@
   ([gen-dims]
     (gen/bind gen-dims
               (fn [dims]
-                (gen/vector (gen-scale #(Math/pow (double %) (/ 1.0 (double dims))) gen/s-pos-int) 
+                (gen/vector (gen/scale #(Math/pow (double %) (/ 1.0 (double dims))) gen/s-pos-int) 
                             dims)))))
 
 (def gen-matrix-shape
