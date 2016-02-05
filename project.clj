f6361e1bb2feeaf3b2053d05e6c26acee7aaa012
Chris Nuernberger <chris@thinktopic.com>
2016-02-05 09:57:16 -0700

    Getting version number correct and making lein install work

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 ;; dependencies / configuration in this file may be out of date
 ;; if in doubt, please refer to the latest pom.xml
 
-(defproject net.mikera/core.matrix "0.44.0"
+(defproject net.mikera/core.matrix "0.45.0-CLJS-SNAPSHOT"
   :url "https://github.com/mikera/core.matrix"
   :license {:name "Eclipse Public License (EPL)"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -14,22 +14,22 @@
 
   :test-paths ["src/test/clojure" "src/test/java"]
 
-  :dependencies [[org.clojure/clojure "1.8.0-beta2"]]
+  :dependencies [[org.clojure/clojure "1.8.0-beta2"]
+                 [net.mikera/cljunit "0.3.1"]]
 
   :marginalia {:javascript ["http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"]}
 
-  :profiles {:dev {:dependencies [[net.mikera/cljunit "0.3.1"]
-                                  [criterium/criterium "0.4.3"]
+  :profiles {:dev {:dependencies [[criterium/criterium "0.4.3"]
                                   [org.clojure/tools.macro "0.1.5"]
                                   [hiccup "1.0.5"]
                                   [clatrix "0.5.0"]
                                   [net.mikera/vectorz-clj "0.37.0"]
-                                  [org.clojure/test.check "0.8.2"]]                      
-                   
+                                  [org.clojure/test.check "0.8.2"]]
+
                    :source-paths ["src/dev/clojure"]
                    :jvm-opts ^:replace []
                    :plugins [[lein-codox "0.9.0"]]}}
-  
+
   :codox {:namespaces [clojure.core.matrix
                        clojure.core.matrix.dataset
                        clojure.core.matrix.io
